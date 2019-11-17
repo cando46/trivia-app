9b81319f8f227f294e895fa7e86b0b264c0c7143
Candogan Ozdemir <ozdemir.candogan@gmail.com>
2019-11-18 01:07:41 +0300

    -Gradle 3.5.2 update, -Library implementations(facebook,materialIO,glide, circular imgv), -Facebook login button (not completed).

--- app/src/main/java/com/mentalsegment/triviaapp/MainActivity.java
+++ app/src/main/java/com/mentalsegment/triviaapp/MainActivity.java
@@ -1,18 +1,68 @@
 package com.mentalsegment.triviaapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        facebookLogin();
+    }
+
+    private void facebookLogin() {
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
     }
 
     private void init() {
+        loginButton=findViewById(R.id.btn_main_fblogin);
+        callbackManager=CallbackManager.Factory.create();
+        loginButton.setPermissions(Arrays.asList("email","public_profile"));
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    //FACEBOOK ACCESS TOKEN TRACER
+    AccessTokenTracker tokenTracker = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+
+        }
+    };
 }
