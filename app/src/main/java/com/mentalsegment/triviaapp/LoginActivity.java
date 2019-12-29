d6d704d87cc6aac22d2ec07607dc47a5bf42c94f
Candogan Ozdemir <ozdemir.candogan@gmail.com>
2019-12-29 16:20:51 +0300

    -icons and menu added

---
+++ app/src/main/java/com/mentalsegment/triviaapp/LoginActivity.java
@@ -0,0 +1,113 @@
+package com.mentalsegment.triviaapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Arrays;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private TextInputLayout usernameWrapper;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+    private TextInputLayout passwordWrapper;
+    Button simpleLoginButton;
+    Intent loginIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        init();
+        facebookLogin();
+    }
+
+
+    private void facebookLogin() {
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+    private void init() {
+       // loginIntent=new Intent(getApplicationContext(),)
+        loginButton = findViewById(R.id.btn_loginPage_fblogin);
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.setPermissions(Arrays.asList("email", "public_profile"));
+        usernameWrapper = findViewById(R.id.usernameWrapper);
+        passwordWrapper = findViewById(R.id.passwordWrapper);
+        simpleLoginButton = findViewById(R.id.btn_loginPage_login);
+        usernameWrapper.setHint("Username");
+        passwordWrapper.setHint("Password");
+        simpleLoginButton=findViewById(R.id.btn_loginPage_login);
+        simpleLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickLoginButton();
+            }
+        });
+    }
+
+    private void onClickLoginButton(){
+        hideKeyboard();
+        if(CheckLogin()){
+        }
+    }
+
+    private boolean CheckLogin() {
+        //it ll be handle
+        return true;
+    }
+
+    private void hideKeyboard() {
+        View view = getCurrentFocus();
+        if (view != null) {
+            ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE)).
+                    hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    //FACEBOOK ACCESS TOKEN TRACER
+    AccessTokenTracker tokenTracker = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+
+        }
+    };
+
+}
